487d27fd9572a69a2c57135bb4ebe5f5971586f6
Aidan Garvey <AidanGarvey@pm.me>
2022-11-19 10:59:03 -0600

    Remove using ~ to halt simulator
    since it can be halted with the pause button, we can allow ~ to be
    treated as a regular character

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,16 +19,9 @@
 
     function keyRelease(event) {
         if(globalThis.simulator && interruptable){
-            // Tilde (~) is used as the interrupt key to force stop a running simulator
-            if(event.key == '~')
-                globalThis.simulator.halt()
-            // Else, send key code to the simulator
-            else
-            {
-                const keyCode = KeyCodes.getAscii(event.key)
-                if (typeof(keyCode) != 'undefined')
-                    globalThis.simulator.keyboardInterrupt(keyCode)
-            }
+            const keyCode = KeyCodes.getAscii(event.key)
+            if (typeof(keyCode) != 'undefined')
+                globalThis.simulator.keyboardInterrupt(keyCode)
             
             reloadOverride.set([true,false])
         }
