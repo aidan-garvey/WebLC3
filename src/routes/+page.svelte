5aae665bbdd5221d465382175c31e500795a7c51
Andrea Abellera <andrea.abellera@gmail.com>
2022-11-21 08:22:17 -0600

    Handle non-printing keycodes
    Consequently handles more keycodes in negative Unicode like âáãä et al

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
     import "../app.css"
     import Header from "../presentation/Header.svelte"
     import Workspace from "../presentation/Workspace.svelte"
-    import { consoleSelected, reloadOverride } from "../presentation/stores"
+    import { consoleSelected, reloadOverride, currentView } from "../presentation/stores"
     import UI from "../presentation/ui"
     import KeyCodes from "../logic/keycodes/keyCodes"
 
@@ -17,7 +17,46 @@
 		interruptable = value
 	});
 
+    // Check if current view is Editor
+    let isEditor = false
+    currentView.subscribe(view => {
+        isEditor = (view == "editor")
+	});
+
+    // Allow page to know if user has unsaved changes
+    let hasUnsaved = false
+    let editorContent = ""
+
+
+    /* Send non-printing keycodes generated in conjunction with CTRL
+     * while preventing browser activity when window is interruptable
+     * Caveat: Cannot send CTRL+N or CTRL+W
+     * Unicode Reference: https://www.physics.udel.edu/~watson/scen103/ascii.html
+     */
+    function keyDown(event){
+        if(event.ctrlKey && interruptable){
+            // Prevent firing of browser shortcuts
+            event.preventDefault()
+
+            // Send modified keycode
+            if(globalThis.simulator){
+                const keyCode = KeyCodes.getAscii(event.key)
+                if (typeof(keyCode) != 'undefined')
+                    globalThis.simulator.keyboardInterrupt(keyCode - 96)
+                
+                reloadOverride.set([true,false])
+            }
+        }
+    }
+
+    /* Send printing keycodes when window is interruptable
+     * Unicode Reference: https://www.physics.udel.edu/~watson/scen103/ascii.html
+     */
     function keyRelease(event) {
+        if(isEditor && globalThis.editor){
+            editorContent = globalThis.editor.getValue()
+        }
+
         if(globalThis.simulator && interruptable){
             const keyCode = KeyCodes.getAscii(event.key)
             if (typeof(keyCode) != 'undefined')
@@ -31,9 +70,23 @@
     function blurConsole(){
 		UI.deselectConsole()
     }
+
+    // TODO: Prompt before closing window if there are unsaved Editor contents
+    function exitPrompt(event){
+        if(hasUnsaved){
+            event.preventDefault();
+		    // Chrome requires returnValue to be set
+		    event.returnValue = "";
+        }
+    }
 </script>
 
-<svelte:window on:keypress={keyRelease} on:click={blurConsole} />
+<svelte:window 
+    on:keydown={keyDown} 
+    on:keypress={keyRelease} 
+    on:click={blurConsole}
+    on:beforeunload={exitPrompt}
+/>
 
 <Header />
 <Workspace />
