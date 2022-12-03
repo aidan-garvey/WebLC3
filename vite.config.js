e855613921baeb5ef21f609b9f7041636a1b4db1
Andrea Abellera <andrea.abellera@gmail.com>
2022-12-03 13:27:26 -0600

    Enable cross origin isolation for Vite
    For COEP, going for require-corp as credentialless doesn't enable isolation for Firefox

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import yaml from "@rollup/plugin-yaml";
 
+function crossOriginIsolationMiddleware(_, response, next) {
+    response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    response.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    response.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+    next();
+}
+
+const crossOriginIsolation = {
+    name: 'cross-origin-isolation',
+    configureServer: server => { server.middlewares.use(crossOriginIsolationMiddleware); },
+    configurePreviewServer: server => { server.middlewares.use(crossOriginIsolationMiddleware); },
+};
+
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		sveltekit(), yaml(),
-		{
-			name: "configure-response-headers",
-			configureServer: (server) => {
-				server.middlewares.use((_req, res, next) => {
-				res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
-				res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-				next();
-				});
-			}
-		}
-	]
+    plugins: [
+        sveltekit(), yaml(), crossOriginIsolation
+    ]
 };
 
 export default config;
